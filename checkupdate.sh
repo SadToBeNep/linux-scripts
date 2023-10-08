713235739c867d69e2369d8705c25c44439d9424
SadToBeNep <62280611+SadToBeNep@users.noreply.github.com>
2023-10-08 16:02:13 +0200

    Revert "Added script to check for updates"
    This reverts commit dbcd48c05eb6e1cfefb2a96169eeb01035f91f19.

--- checkupdate.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-#This script requires apt update AND apt list --upgradeable to work without sudo
-#This can be done the easiest by adding %{USERNAME} ALL=NOPASSWD: /bin/apt update, /bin/apt list upgradable to the sudoers file
-
-sudo apt update > /dev/null
-OUTPUT=$(sudo apt list --upgradable | wc -l)
-echo "" $OUTPUT
-
