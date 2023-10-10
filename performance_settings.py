94dfeccf8704c0514b0ae3748847035d5ad87bd2
SadToBeNep <62280611+SadToBeNep@users.noreply.github.com>
2023-10-10 07:01:20 +0200

    Fixing lint errors

--- performance_settings.py
+++ performance_settings.py
@@ -1,33 +1,32 @@
 #!/bin/python3
+import subprocess
+import sys
+
 
 ## This script was made to be used with polybar
 POWERSAVE_GLYPH = " 󱙷 "
 PERFORMANCE_GLYPH = " 󰲉 "
-import subprocess,sys
+
 
 def read_current_setting():
-    cpu_speed = subprocess.run(['/home/nep/Scripts/read_cpu_ghz.sh'],capture_output=True,text=True)
+    cpu_speed = subprocess.run(['/home/nep/Scripts/read_cpu_ghz.sh'],
+                               capture_output=True,text=True,check=False)
+
     cpu_speed = cpu_speed.stdout.strip()
 
+    return_data = subprocess.run(['cat','/sys/devices/system/cpu/cpu0/cpufreq/scaling_governor'], 
+                                 capture_output=True,text=True,check=False)
 
-    return_data = subprocess.run(['cat','/sys/devices/system/cpu/cpu0/cpufreq/scaling_governor'], capture_output=True,text=True)
-    if("powersave" in return_data.stdout):
-        print(f"{POWERSAVE_GLYPH} {cpu_speed}")
-    else:
-        print(f"{PERFORMANCE_GLYPH} {cpu_speed}")
-    
+    if("powersave" in return_data.stdout): print(f"{POWERSAVE_GLYPH} {cpu_speed}")
+    else: print(f"{PERFORMANCE_GLYPH} {cpu_speed}")
 
 def change_to_another_mode():
-    return_data = subprocess.run(['cat','/sys/devices/system/cpu/cpu0/cpufreq/scaling_governor'], capture_output=True,text=True)
-    if("powersave" in return_data.stdout):
-        subprocess.run(['sudo','to_performance'])
-        
-    else:
-        subprocess.run(['sudo','to_powersave'])
+    return_data = subprocess.run(['cat','/sys/devices/system/cpu/cpu0/cpufreq/scaling_governor'], 
+                                 capture_output=True,text=True,check=False)
+    if("powersave" in return_data.stdout): subprocess.run(['sudo','to_performance'],check=False)
+    else:subprocess.run(['sudo','to_powersave'],check=False)
 
 if(__name__ == "__main__"):
-    if(len(sys.argv) == 2):
-        change_to_another_mode()
-    else:
-        read_current_setting()
+    if(len(sys.argv) == 2): change_to_another_mode()
+    else: read_current_setting()
     
--- secup.py
+++ secup.py
@@ -4,7 +4,7 @@
 # The stdout of the script is the link to the file
 #
 # 0x0.st is a simple website where I can upload my files in case I want to share something with someone
-# and for example the file would be too big for a discord share, or I just simply want to save a file 
+# and for example the file would be too big for a discord share, or I just simply want to save a file
 # for later use somewhere else on another computer
 #
 #
@@ -12,57 +12,70 @@
 # PROGRAM PATH_TO_FILE_OR_FOLDER_TO_UPLOAD
 ####################################
 
-import os, subprocess, sys, uuid
+import os
+import subprocess
+import sys
+import uuid
+
 TEMP_DIR_PATH = "/tmp/secup"
 
 
 def create_temp_dir():
-    
+
     # Create directory to work in a separated space
-    return_code = subprocess.run(['mkdir',TEMP_DIR_PATH], capture_output=True, text=True)
-    
+    return_code = subprocess.run(['mkdir', TEMP_DIR_PATH],
+                                 capture_output=True, text=True, check=False)
+
     # Basic error handling
-    if(return_code.returncode == 1):
+    if (return_code.returncode == 1):
         print(f"Error while creating temp directory: \n{return_code.stderr}")
+
 
 def collect_files_at_temp():
     create_temp_dir()
     FILE_NAME = sys.argv[1]
     IS_IT_DIR = os.path.isdir
-    if(IS_IT_DIR):
-        return_code = subprocess.run(['cp','-r',FILE_NAME,TEMP_DIR_PATH], capture_output=True,text=True)
+    if (IS_IT_DIR):
+        return_code = subprocess.run(['cp', '-r', FILE_NAME, TEMP_DIR_PATH],
+                                     capture_output=True, text=True, check=False)
     else:
-        return_code = subprocess.run(['cp',FILE_NAME,TEMP_DIR_PATH],capture_output=True,text=True)
-    if(return_code.returncode == 1):
+        return_code = subprocess.run(['cp', FILE_NAME, TEMP_DIR_PATH],
+                                     capture_output=True, text=True, check=False)
+
+    if (return_code.returncode == 1):
         print(f"Error happened while moving files: \n{return_code.stderr}")
     else:
         zip_collected_files()
-    
+
 
 def zip_collected_files():
     ZIP_FILE_NAME = create_filename()
-    return_code = subprocess.run(['zip','-r','-e',ZIP_FILE_NAME+".zip",TEMP_DIR_PATH+"/"],capture_output=True,text=True)
+    return_code = subprocess.run(['zip', '-r', '-e', ZIP_FILE_NAME+".zip", TEMP_DIR_PATH+"/"],
+                                 capture_output=True, text=True, check=False)
     print(return_code)
-    if(return_code.returncode == 1):
+    if (return_code.returncode == 1):
         print(f"Error: \n{return_code.stderr}")
     upload_zip_file(ZIP_FILE_NAME)
 
+
 def upload_zip_file(ZIP_FILE_NAME):
     LCG = TEMP_DIR_PATH+"/"+ZIP_FILE_NAME+".zip"
-    
-    subprocess.run(['mv',ZIP_FILE_NAME+".zip",TEMP_DIR_PATH+"/."])
-    return_code = subprocess.run(['curl','-F',f"file=@{LCG}",'http://0x0.st'],capture_output=True,text=True)
+
+    subprocess.run(['mv', ZIP_FILE_NAME+".zip", TEMP_DIR_PATH+"/."])
+    return_code = subprocess.run(['curl', '-F', f"file=@{LCG}", 'http://0x0.st'],
+                                 capture_output=True, text=True, check=False)
     print(return_code.stdout)
     cleanup()
 
-def cleanup():
-    subprocess.run(['rm','-rf',TEMP_DIR_PATH])
 
-    
+def cleanup():
+    subprocess.run(['rm', '-rf', TEMP_DIR_PATH], check=False)
+
+
 def create_filename():
     UUID = str(uuid.uuid1()).split('-')[0]
     return (UUID)
 
-if(__name__ == "__main__"):
+
+if (__name__ == "__main__"):
     collect_files_at_temp()
-    
