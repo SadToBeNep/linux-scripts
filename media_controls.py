68bdcdfb1b928681fc8cc7ad3f27da9c6b6429cf
SadToBeNep <62280611+SadToBeNep@users.noreply.github.com>
2023-10-10 08:54:35 +0200

    Fixed output error in polybar

--- media_controls.py
+++ media_controls.py
@@ -3,6 +3,7 @@
 """Used to execute commands"""
 import subprocess
 import re
+import time
 
 ## This script was made to be used with polybar ##
 # Requires playerctl to be installed
@@ -18,33 +19,32 @@ try:
     ARTIST = returned_data[3]
     TITLE = returned_data[1]
     MAX_CHARACTERS = 35
+
+    match = re.search(TITLE_PATTERN, TITLE)
+
+    # Check if a match was found
+    if match:
+        song_title = match.group(1)
+        TITLE = song_title.strip()
+    else:
+        print("No match found.")
+
+
+
+    match = re.search(ARTIST_PATTERN, ARTIST)
+
+    # Check if a match was found
+    if match:
+        song_artist = match.group(1)
+        ARTIST = song_artist.strip()
+    else:
+        print("No match found.")
+
+
+
+    OUTPUT = f"{ARTIST} - {TITLE}"
+    if len(OUTPUT) > MAX_CHARACTERS:
+        OUTPUT = OUTPUT[0:MAX_CHARACTERS]
+    print(OUTPUT)
 except IndexError:
     print("-*-")
-
-
-match = re.search(TITLE_PATTERN, TITLE)
-
-# Check if a match was found
-if match:
-    song_title = match.group(1)
-    TITLE = song_title.strip()
-else:
-    print("No match found.")
-
-
-
-match = re.search(ARTIST_PATTERN, ARTIST)
-
-# Check if a match was found
-if match:
-    song_artist = match.group(1)
-    ARTIST = song_artist.strip()
-else:
-    print("No match found.")
-
-
-
-OUTPUT = f"{ARTIST} - {TITLE}"
-if len(OUTPUT) > MAX_CHARACTERS:
-    OUTPUT = OUTPUT[0:MAX_CHARACTERS]
-print(OUTPUT)
