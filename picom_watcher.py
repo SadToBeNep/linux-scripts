77ace7171eb6b858a9c387bda84376a899efa43e
SadToBeNep <62280611+SadToBeNep@users.noreply.github.com>
2023-11-22 13:16:19 +0100

    Fixed picom_watcher

--- picom_watcher.py
+++ picom_watcher.py
@@ -4,9 +4,11 @@ import json
 import subprocess
 from time import sleep
 from os import mkdir,path
+import psutil
 
 class ConfigFile():
 	BASIC_CONFIG = '{"watchout_list": ["Guild Wars 2", "League of Legends", "Riot Client"], "sleep_time": 15, "compozitor": "picom", "config_location": "~/.config/pi_watch/config"}'
+	NO_REC = False
 
 	def __init__(self, watchout_list=["Guild Wars 2", "Riot Client", "League of Legends"], sleep_time=10, compozitor="picom", config_location="~/.config/pi_watch/config"):
 		self.watchout_list = watchout_list
@@ -20,8 +22,10 @@ class ConfigFile():
 	
 	def OpenConfigFile(self):
 		try:
-			ConfigContents = self.ReadConfigFile()
-			self.__dict__ == json.loads(ConfigContents)
+			if(self.NO_REC == False):
+				print(self.NO_REC)
+				ConfigContents = self.ReadConfigFile()
+				self.__dict__ == json.loads(ConfigContents)
 
 		except FileNotFoundError:
 			print(f"No config file was found at {self.config_location}\nDo you want to create one? [Y/n]")
@@ -31,7 +35,7 @@ class ConfigFile():
 			else:
 				print("[ERR] No config file found, exit")
 				exit(404)
-		self.OpenConfigFile()
+			self.OpenConfigFile()
 			
 	def CreateConfigFile(self):
 		CreateDir = True
@@ -64,33 +68,53 @@ class ConfigFile():
 		with open(f"/home/{Username}/.config/pi_watch/config","r") as op:
 			x = op.readline()
 			op.close()
+			self.NO_REC = True
 			return x
 				
 
 
-watchout = ["Guild Wars 2","League of Legends","Riot Client"]
-killpicom = False
-something = ConfigFile()
+def is_process_running(process_name):
+	for process in psutil.process_iter(['pid', 'name']):
+		#print(process.info['name'])
+		if process.info['name'] == process_name:
+	            return True
+	return False
 
-something.OpenConfigFile()
+def kill_process(process_name):
+    try:
+        for process in psutil.process_iter(['pid', 'name']):
+            if process.info['name'] == process_name:
+                pid = process.info['pid']
+                subprocess.run(['kill', str(pid)])
+                print(f"Killed process: {process_name} (PID: {pid})")
+    except Exception as e:
+        print(f"Error while killing process {process_name}: {e}")
 
-print(something.compozitor)
+# Replace with the actual names of the processes you're looking for
+process_names_to_check = ["Gw2-64.exe", "League of Legends", "Riot Client"]
+picom_process_name = "picom"
 
-print(json.dumps(something.__dict__))
+while(1):
+	# Check if any of the specified processes are running
+	for process_name in process_names_to_check:
+		if is_process_running(process_name):
+			print(f"Process {process_name} is running.")
+			# If any of the specified processes are running, kill picom
+			kill_process(picom_process_name)
+			break
+		else:
+			print(f"Process {process_name} is not running.")
 
-while(0):
-	for x in watchout:
-		output1 = subprocess.Popen(["ps","aux"],stdout=subprocess.PIPE)
-		output2 = subprocess.Popen(["grep",x],stdin=output1.stdout,stdout=subprocess.PIPE,stderr=subprocess.PIPE)
-		output1.stdout.close()
-		out,err = output2.communicate()
-		out = out.decode("utf-8")
-		print(out)
-		if len(out.split("\n")) > 2:
-			killpicom = True
-	if killpicom:
-		subprocess.Popen(["killall","picom"])
-		killpicom = False
+	# If none of the specified processes are running, check if picom is running
+	if not any(is_process_running(process_name) for process_name in process_names_to_check):
+		if not is_process_running(picom_process_name):
+			# If picom is not running, start picom
+			subprocess.Popen(['nohup','picom'])
+			print("Started picom.")
+		else:
+			print("picom is already running.")
 	else:
-		subprocess.Popen(["nohup","picom"])
+		print("picom not started, as at least one of the specified processes is running.")
+
+	print("Script execution completed.")
 	sleep(10)
